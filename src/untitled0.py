af0ba97eb4f40b5ca7c3f8717124bc55d15eda28
Rafael Lúcio <rafaellucio@pm.me>
2021-03-28 18:40:26 -0300

    renamed 'report_dates' to 'sample_dates' and excluded useless function

--- src/untitled0.py
+++ src/untitled0.py
@@ -12,18 +12,10 @@ import datetime as dt
 
 # Finds sample data in function of the script folder
 script_directory = os.getcwd()
-project_directory = os.path.abspath(os.path.join(script_directory, 
+project_directory = os.path.abspath(os.path.join(script_directory,
                              os.pardir))
 
 
 # List every report name folder name to be treated afterwards
-report_dates = os.listdir(f'{project_directory}/sample_data')
+sample_dates = os.listdir(f'{project_directory}/sample_data')
 
-def convert_string_to_date_object(str_list, date_format):
-    aux = []
-    for date in str_list:
-        aux.append(dt.datetime.strptime(date, date_format))
-        
-    return aux
-
-report_dates = convert_string_to_date_object(report_dates, '%Y-%m-%d')
