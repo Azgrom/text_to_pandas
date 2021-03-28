75b7bf98dc29768e4629d676ff862d5c4bdc1375
Rafael Lúcio <rafaellucio@pm.me>
2021-03-28 19:11:12 -0300

    file to dataframe converter

--- src/untitled0.py
+++ src/untitled0.py
@@ -8,6 +8,7 @@ Created on Sun Mar 28 18:02:17 2021
 
 import os
 import datetime as dt
+import pandas as pd
 
 
 # Finds sample data in function of the script folder
@@ -15,7 +16,25 @@ script_directory = os.getcwd()
 project_directory = os.path.abspath(os.path.join(script_directory,
                              os.pardir))
 
+sample_data_path = f'{project_directory}/sample_data'
 
-# List every report name folder name to be treated afterwards
-sample_dates = os.listdir(f'{project_directory}/sample_data')
+data_structure = {
+    'title': [],
+    'author': [],
+    'date_published': [],
+    'link': [],
+    'content': []
+}
 
+# Loop over files
+for root, _, files in os.walk(sample_data_path):
+    for report in files:
+        with open(f'{root}/{report}', 'rt') as f:
+            data_structure['title'].append(f.readline())
+            data_structure['author'].append(f.readline())
+            data_structure['date_published'].append(f.readline())
+            data_structure['link'].append(f.readline())
+            data_structure['content'].append(f.readlines())
+
+
+data_structure = pd.DataFrame(data_structure)
